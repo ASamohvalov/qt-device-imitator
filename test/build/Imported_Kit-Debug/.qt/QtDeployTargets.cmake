f2f3fefe4ff7ad26a649e08eafa4e592d1d7dda3
artem <artym.samohvaloff@gmail.com>
2025-12-14 20:56:51 +0400

    gitingore added in test

--- test/build/Imported_Kit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_device-imitators-test_FILE /home/artem/Documents/programming/languages/c++/qt/qt-device-imitator/test/build/Imported_Kit-Debug/device-imitators-test)
-set(__QT_DEPLOY_TARGET_device-imitators-test_TYPE EXECUTABLE)
